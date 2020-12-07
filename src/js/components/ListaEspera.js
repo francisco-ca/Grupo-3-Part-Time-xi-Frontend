dd7c80769444bbb7317c08166f7570e340b17a3a
francisco-ca <francisco4_1996@hotmail.com>
2020-12-07 06:36:42 +0000

    listaesperaavanzada

--- src/js/components/ListaEspera.js
+++ src/js/components/ListaEspera.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../store/appContext';
 import NavbarTop from './NavbarTop';
 import { Button, Card, Container, Jumbotron, Col, Row, Image } from 'react-bootstrap';
+import "./listaespera.css"
 
 
 const ListaEspera = (props) => {
@@ -19,14 +20,14 @@ console.log("props",props)
     return (
         <Container>
             <NavbarTop />
-            <Jumbotron fluid className="pt-3 pb-3 text-center" >
+            <Jumbotron fluid className="pt-3 pb-3 text-center fondo " style={{ height: '55rem' }}>
                 <Container >
                     <h1>{store.restaurante.nombre}</h1>
                     <hr></hr>
-                    <Row>
-                        <Col md={{ size: 'auto' }}>
+                    <Row >
+                        <Col className="pb-2" md={{ size: 'auto' }}>
 
-                            <p className='text-justify '>
+                            <p className='text-justify pt-2 pb-3 '>
                                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
                                 doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
                                 veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
@@ -34,26 +35,28 @@ console.log("props",props)
                                 magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
                                 qui dolorem ipsum quia dolor sit amet, consectetur
                     </p>
+                    
+ <hr></hr>
+                            <Image className="img" src="/restaurante1.jpg" rounded fluid />
+                            <hr></hr>
 
-                            <Image src="/restaurante1.jpg" rounded fluid />
+                     <h3 className="pt-5">Mesas disponibles: {0}/{store.restaurante.cantidad_maxima}</h3>
                         </Col>
                         <Col md={6}>
-                            <Card className='mx-auto p-3 mb-2 bg-dark text-white text-left' style={{ width: '30rem' }}>
+                            <Card className='mx-auto p-3 mb-2 lista text-white text-left' style={{ width: 'auto', height:"45rem" }}>
                                 <Card.Body>
+                                    
                                     <Card.Title >
-                                        <h3>Mesas disponibles: {0}/{store.restaurante.cantidad_maxima}</h3>
+                                        <h3 className="text-center pt-5 pl-3">Lista de espera: {5}/{store.restaurante.capacidad_lista_espera} personas</h3>
                                     </Card.Title>
-                                    <Card.Title className='my-4'>
-                                        <h3>Lista de espera: {5}/{store.restaurante.capacidad_lista_espera} personas</h3>
-                                    </Card.Title>
-                                    <Card.Text>
+                                    <Card.Text >
                                         {(inList) ?
                                             "" : <h4>Tu lugar en la lista es: 3</h4>
                                         }
                                     </Card.Text>
 
-                                    <form className='my-4' action='#' method='PUT' onSubmit={(e) => { e.preventDefault(); }}>{/* ruta en la api que maneje este form */}
-                                        <Button variant="primary" size="lg" type='submit' onClick={() => { setInList(!inList) }}/*que mande tipo put para modificar la lista */>{inList ? 'Anotate en la fila!' : 'Salir de la fila'}</Button>
+                                    <form className='my-5 abajo ' action='#' method='PUT' onSubmit={(e) => { e.preventDefault(); }}>{/* ruta en la api que maneje este form */}
+                                        <Button variant="primary " size="lg" type='submit' onClick={() => { setInList(!inList) }}/*que mande tipo put para modificar la lista */>{inList ? 'Anotate en la fila!' : 'Salir de la fila'}</Button>
                                     </form>
                                 </Card.Body>
                             </Card>
