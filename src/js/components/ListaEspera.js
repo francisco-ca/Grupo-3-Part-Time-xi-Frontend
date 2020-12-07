514028b2a3b296cbc8eb1b00e3886a510c34919d
francisco-ca <francisco4_1996@hotmail.com>
2020-12-07 18:36:38 +0000

    lista de esprea estilizada

--- src/js/components/ListaEspera.js
+++ src/js/components/ListaEspera.js
@@ -20,14 +20,14 @@ console.log("props",props)
     return (
         <Container>
             <NavbarTop />
-            <Jumbotron fluid className="pt-3 pb-3 text-center fondo " style={{ height: '55rem' }}>
+            <Jumbotron fluid className="pt-3 pb-3 text-center fondo " style={{ height: 'auto' }}>
                 <Container >
-                    <h1>{store.restaurante.nombre}</h1>
+                    <h1 className="font">{store.restaurante.nombre}</h1>
                     <hr></hr>
                     <Row >
-                        <Col className="pb-2" md={{ size: 'auto' }}>
+                        <Col className="pt-2 pb-2" md={{ size: 'auto' }}>
 
-                            <p className='text-justify pt-2 pb-3 '>
+                            <p className='text-justify borde font p-3 '>
                                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
                                 doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
                                 veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
@@ -37,26 +37,26 @@ console.log("props",props)
                     </p>
                     
  <hr></hr>
-                            <Image className="img" src="/restaurante1.jpg" rounded fluid />
+                            <Image className="borde" src="/restaurante1.jpg"  fluid />
                             <hr></hr>
 
-                     <h3 className="pt-5">Mesas disponibles: {0}/{store.restaurante.cantidad_maxima}</h3>
+                     <h3 className=" p-3 mesas font ">Mesas disponibles: {0}/{store.restaurante.cantidad_maxima}</h3>
                         </Col>
                         <Col md={6}>
                             <Card className='mx-auto p-3 mb-2 lista text-white text-left' style={{ width: 'auto', height:"45rem" }}>
                                 <Card.Body>
                                     
                                     <Card.Title >
-                                        <h3 className="text-center pt-5 pl-3">Lista de espera: {5}/{store.restaurante.capacidad_lista_espera} personas</h3>
+                                        <h3 className="text-center font pt-2 pl-3">Lista de espera: {5}/{store.restaurante.capacidad_lista_espera} </h3>
                                     </Card.Title>
-                                    <Card.Text >
+                                    <Card.Text className="font" >
                                         {(inList) ?
                                             "" : <h4>Tu lugar en la lista es: 3</h4>
                                         }
                                     </Card.Text>
 
-                                    <form className='my-5 abajo ' action='#' method='PUT' onSubmit={(e) => { e.preventDefault(); }}>{/* ruta en la api que maneje este form */}
-                                        <Button variant="primary " size="lg" type='submit' onClick={() => { setInList(!inList) }}/*que mande tipo put para modificar la lista */>{inList ? 'Anotate en la fila!' : 'Salir de la fila'}</Button>
+                                    <form className='my-5 abajo text-center font' action='#' method='PUT' onSubmit={(e) => { e.preventDefault(); }}>{/* ruta en la api que maneje este form */}
+                                        <Button variant="dark" className="boton" size="lg" type='submit' onClick={() => { setInList(!inList) }}/*que mande tipo put para modificar la lista */>{inList ? 'Anotate en la fila!' : 'Salir de la fila'}</Button>
                                     </form>
                                 </Card.Body>
                             </Card>
